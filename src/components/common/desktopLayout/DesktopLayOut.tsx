f33847b8d4bec9a3200dd03a21e4e33e9b39fd83
Okwoli-Godwin <okwolig60@gmail.com>
2023-07-18 14:30:18 +0100

    home sidebar added

--- src/components/common/desktopLayout/DesktopLayOut.tsx
+++ src/components/common/desktopLayout/DesktopLayOut.tsx
@@ -1,11 +1,20 @@
 
 import { Outlet } from 'react-router-dom'
 import DestopFooter from '../../static/DestopFooter'
+import RightSidebar from '../../static/RightSidebar'
+import LeftSidebar from '../../static/LeftSidebar'
 
 const DesktopLayOut = () => {
     return (
-        <div>
+        <div
+            style={{
+            display: "flex",
+            width: "100%"
+        }}
+        >
+            <LeftSidebar />
             <Outlet />
+            <RightSidebar />
             <DestopFooter />
         </div>
     )
