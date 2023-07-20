a8556e0203ea6d36aabda51661631e36d719e825
Dc Valerian <Dc-Valerian>
2023-07-20 14:29:35 +0100

    added

--- src/pages/desktop/NotificationPage.tsx
+++ src/pages/desktop/NotificationPage.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+
+import LeftSidebar from '../../components/static/LeftSidebar'
+import {FiSettings} from "react-icons/fi"
 
 const NotificationPage = () => {
   return (
-    <div>NotificationPage</div>
+ <div className="flex w-[100%]">
+     <LeftSidebar/>
+     {/* NOTIFICTION PAGE STYLING */}
+  <div className='w-[100%] bg-[red] h-[400px] justify-end flex'>
+    <div className='w-[90%] bg-[green] h-[50px]' style={{ width: "calc(100% - 18%)" }}>
+        Notifications
+        <FiSettings/>
+    </div>
+  </div>
+ </div>
   )
 }
 
