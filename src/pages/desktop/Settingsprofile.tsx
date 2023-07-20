afc28a76895984a174e2cdc8f3ec68f0d3edd23a
Okwoli-Godwin <okwolig60@gmail.com>
2023-07-20 17:05:18 +0100

    account center done

--- src/pages/desktop/Settingsprofile.tsx
+++ src/pages/desktop/Settingsprofile.tsx
@@ -1,4 +1,5 @@
 import LeftSidebar from "../../components/static/LeftSidebar"
+import Accountcenter from "./Accountcenter"
 
 
 const Settingsprofile = () => {
@@ -9,7 +10,16 @@ const Settingsprofile = () => {
           {/* profile editting settings */}
           <div className="w-[100%] justify-end flex">
               <main className="bg-white h-[100%] column justify-center items-center pt-[40px] pl-[20px]" style={{ width: "calc(100% - 18%)" }}>
-                  bbdududu
+                  <h3 className="text-[23px]">Settings</h3>
+
+                  <div className="w-[100%] h-[100%] flex justify-center items-center">
+                      <div className="w-[90%] h-[100vh] border-[#DBDBDB] border rounded-md flex">
+                          
+                          <div className="w-[25%] flex column border-[#DBDBDB] border-r p-[18px]">
+                              <Accountcenter />
+                          </div>
+                      </div>
+                  </div>
               </main>
           </div>
     </div>
