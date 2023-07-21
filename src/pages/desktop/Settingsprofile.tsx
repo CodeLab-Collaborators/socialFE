a0b4ef080865a1c5146ab69e35b88d2480b2b16c
Okwoli-Godwin <okwolig60@gmail.com>
2023-07-21 05:53:58 +0100

    update

--- src/pages/desktop/Settingsprofile.tsx
+++ src/pages/desktop/Settingsprofile.tsx
@@ -1,5 +1,6 @@
 import LeftSidebar from "../../components/static/LeftSidebar"
 import Accountcenter from "./Accountcenter"
+import Accounteditcenter from "./auth/Accounteditcenter"
 
 
 const Settingsprofile = () => {
@@ -9,14 +10,15 @@ const Settingsprofile = () => {
 
           {/* profile editting settings */}
           <div className="w-[100%] justify-end flex">
-              <main className="bg-white h-[100%] column justify-center items-center pt-[40px] pl-[20px]" style={{ width: "calc(100% - 18%)" }}>
+              <main className="bg-white column justify-center items-center pt-[40px] pl-[20px] pb-[50px]" style={{ width: "calc(100% - 18%)" }}>
                   <h3 className="text-[23px]">Settings</h3>
 
                   <div className="w-[100%] h-[100%] flex justify-center items-center">
                       <div className="w-[90%] h-[100vh] border-[#DBDBDB] border rounded-md flex">
                           
-                          <div className="w-[25%] flex column border-[#DBDBDB] border-r p-[18px]">
+                          <div className="w-[25%] column border-[#DBDBDB] border-r p-[18px]">
                               <Accountcenter />
+                              <Accounteditcenter />
                           </div>
                       </div>
                   </div>
