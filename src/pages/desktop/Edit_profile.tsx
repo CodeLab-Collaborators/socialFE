c1d17da8b44c15fe62d7a03cce70c744b11a565f
Okwoli-Godwin <okwolig60@gmail.com>
2023-07-21 15:52:34 +0100

    editing profile update

---
+++ src/pages/desktop/Edit_profile.tsx
@@ -0,0 +1,11 @@
+
+
+const Edit_profile = () => {
+    return (
+        <div className="column items-center">
+            <h1 className="text-[20px] w-[100%]">Edit profile</h1>
+        </div>
+    )
+}
+
+export default Edit_profile
