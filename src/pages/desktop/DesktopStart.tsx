36e0e624e1bc21c33616046a462db6c601f0ffbe
Okwoli-Godwin <okwolig60@gmail.com>
2023-07-28 14:58:10 +0100

    update

--- src/pages/desktop/DesktopStart.tsx
+++ src/pages/desktop/DesktopStart.tsx
@@ -1,4 +1,5 @@
 import Postfile from "./Postfile"
+import img from "../../assets/chelsea.jpg"
 
 const DesktopStart = () => {
     return (
@@ -14,8 +15,12 @@ const DesktopStart = () => {
 
                <Postfile />
 
-            <div className="w-[100%] h-[100vh] bg-[red]">
-                floran
+            <div className="w-[100%] h-[100vh] pr-[20px] pl-[20px] pt-[15px] flex-col">
+                <div className="w-[100%] flex h-[40px]">
+                    <div className="w-[55px] h-[55px] rounded-full overflow-hidden">
+                        <img src={img} alt="" className="w-[100%] h-[100%] object-cover"/>
+                    </div>
+                </div>
             </div>
         </div>
     )
